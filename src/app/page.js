86b3baf5475b517b5a9ba1753bcb4b66a951bff4
kaleckh <69163050+kaleckh@users.noreply.github.com>
2023-06-16 16:37:51 -0600

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,247 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useRef, useEffect } from "react";
+// import Header from "./header/page";
+import Head from "next/head";
+// import Footer from "./footer/page";
+import Image from "next/image";
+import { PatternFormat } from "react-number-format";
+import styles from "./page.module.css";
+// import { useRouter } from "next/router";
+import ReCAPTCHA from "react-google-recaptcha";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+import { useState } from "react";
+const Home = () => {
+  const [recaptchaResponse, setRecaptchaResponse] = useState(false);
+  const tawkMessengerRef = useRef();
+  const [gray, setGray] = useState(true);
+  const [grayBottom, setGrayBottom] = useState(true);
+  const [brandDescription, setBrandDescription] = useState();
+  const [quote, setQuote] = useState(false);
+  const [model, setModel] = useState();
+  const [description, setDescription] = useState();
+  const [image, setImage] = useState();
+  const [type, setType] = useState();
+  const [timeOut, setTimeOut] = useState();
+  const [printSpeed, setPrintSpeed] = useState();
+  const [paperSize, setpaperSize] = useState();
+  const [modelNumber, setModelNumber] = useState();
+  const [lastBullet, setLastBullet] = useState();
+  const [almostLastBullet, setAlmostLastBullet] = useState();
+  const handleMinimize = () => {
+    tawkMessengerRef.current.minimize();
+  };
+  const onLoad = () => {
+    console.log("onLoad works!");
+  };
+  var verifyCallback = function (response) {
+    setRecaptchaResponse(response);
+  };
+  const captchaRef = useRef(null);
+  useEffect(() => {
+    const storedModel = localStorage.getItem("Model");
+    const photo = localStorage.getItem("Image");
+    const time = localStorage.getItem("timeOut");
+    const back = localStorage.getItem("type");
+    const speed = localStorage.getItem("PagesPerMinute");
+    const modelNumber = localStorage.getItem("modelNumber");
+    const paperSize = localStorage.getItem("paperSize");
+    const desc = localStorage.getItem("description");
+    setModel(storedModel);
+    setImage(photo);
+    setType(back);
+    setPrintSpeed(speed);
+    setModelNumber(modelNumber);
+    setpaperSize(paperSize);
+    setTimeOut(time);
+    setDescription(desc);
 
-export default function Home() {
+    if (localStorage.getItem("brand") === "lexmark") {
+      setBrandDescription(
+        "Lexmark, formerly an IBM company, had produced hands down the most reliable machines ever built. Their modular construction ensures the most efficient paper path in the industry. Independent BLI testing proved their top copier models performing with only 1 jam after 1,000,000 copies tested. Their dominant 85% of the market share in pharmacuetical and medical establishments is a testament to their unrivaled reliability"
+      );
+      setLastBullet(
+        " Known for their fast print speeds and efficient performance."
+      );
+      setAlmostLastBullet(
+        "Durability and reliability: Lexmark copiers are built to withstand heavy use and are known for their reliability, reducing downtime and ensuring consistent performance."
+      );
+    } else if (localStorage.getItem("brand") !== "lexmark") {
+      setLastBullet(
+        "High-quality output: Konica Minolta copiers are known for delivering exceptional print quality with sharp text and vibrant images."
+      );
+      setBrandDescription(
+        "Konica Minolta offers the top color quality output in the industry. These machines are built to last with minimal disruption. With up to 12x18 paper sizes standard, there is nothing your office cannot do with these copiers."
+      );
+      setAlmostLastBullet(
+        "Energy efficiency: Konica Minolta copiers are designed with energy-saving features, helping businesses reduce their environmental footprint and save on energy costs."
+      );
+    }
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.main}>
+    
+      <Head>
+        <title>
+          New and Used Lexmark Copiers | High-Quality and Affordable | Copiers
+          Utah
+        </title>
+        <meta
+          name="description"
+          content="Copiers Utah offers high-quality and affordable Lexmark copiers, both new and used. Learn more about Lexmark copiers and how they can benefit your office. Fill out our quote form to receive a customized quote."
+        />
+        <meta
+          name="keywords"
+          content="Lexmark copiers, used Lexmark copiers, new Lexmark copiers, office copiers, copiers Utah, affordable copiers"
+        />
+      </Head>
+      <div>
+        <TawkMessengerReact
+          onLoad={onLoad}
+          propertyId="5abd4931d7591465c7090c65"
+          widgetId="default"
+          useRef={tawkMessengerRef}
+        />
+      </div>
+      <div className={styles.logoSpaceContainer}>
+        <div className={styles.logoSpace}>
+          <Image
+            src={`/static/logo.webp`}
+            alt="Lexmark used and new printer"
+            width={150}
+            height={100}
+          />
+          <div className={styles.columnContainer}>
+            <div />
+            <div className={styles.infoBig}>Copiers Utah</div>
+            <div className={styles.mediumColumn}>
+              <div className={styles.infoSmall}>info@copiersutah.com</div>
+              <div className={styles.infoMedium}>Ph: (801) 261-0510</div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* <Header /> */}
+
+      <div
+        style={{
+          height: "fit-content",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "space-around",
+        }}
+      >
+        <div className={styles.backContainer}>
+          <div
+            onClick={() => {
+              router.push(`/${type}`);
+            }}
+            style={{
+              padding: "5px",
+              fontSize: "25px",
+              fontWeight: "400",
+              cursor: "pointer",
+              borderRadius: "100%",
+            }}
+          >
+            {" "}
+            X
+          </div>
+        </div>
+        <div className={styles.lineColumn}>
+          <div className={styles.color}>{}</div>
+          <div style={{ width: "150%" }} className={styles.line} />
+        </div>
+        <div className={styles.row}>
+          <div className={styles.copierContainer}>
+            <Image src={`/static/${image}`} width={200} height={150} />
+            <button
+              style={{ width: "100%" }}
+              onClick={() => {
+                router.push("/buy");
+              }}
+              className={styles.button}
+            >
+              Request a quote
+            </button>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <div style={{ padding: "5px" }} className={styles.title}>
+                Model Number:
+              </div>
+              <div className={styles.title}>{modelNumber}</div>
+            </div>
+          </div>
+          {quote ? (
+            <Form />
+          ) : (
+            <div className={styles.column}>
+              <div className={styles.bigTitle}>{model}</div>
+              <div className={styles.aboutRow}>
+                <div
+                  onClick={() => {
+                    setGray(true);
+                  }}
+                  className={
+                    gray ? `${styles.focusTitleGray}` : `${styles.focusTitle}`
+                  }
+                >
+                  About
+                </div>
+              </div>
+              <div className={styles.line}></div>
+              <div>
+                <div>
+                  <div className={styles.bulletContainer}>
+                    <div className={styles.bullet}>1s</div>
+                    <div className={styles.paragraphSmall}>
+                      Print speeds at {printSpeed} pages per minute!
+                    </div>
+                  </div>
+                  <div className={styles.bulletContainer}>
+                    <div className={styles.bullet}>1s</div>
+                    <div className={styles.paragraphSmall}>
+                      Largest print size is {paperSize} inches
+                    </div>
+                  </div>
+                  <div className={styles.bulletContainer}>
+                    <div className={styles.bullet}>1s</div>
+                    <div className={styles.paragraphSmall}>
+                      First page out time is {timeOut} seconds
+                    </div>
+                  </div>
+                  <div className={styles.bulletContainer}>
+                    <div className={styles.bullet}>1s</div>
+                    <div className={styles.paragraphSmall}>{lastBullet}</div>
+                  </div>
+                  <div className={styles.bulletContainer}>
+                    <div className={styles.bullet}>1s</div>
+                    <div className={styles.paragraphSmall}>
+                      {almostLastBullet}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+        <div style={{ height: "40%" }} className={styles.konikaBottom}>
+          <div className={styles.bottomProductContainer}>
+            <div className={styles.paragraph}>{brandDescription}</div>
+            <div className={styles.paragraph}>{description}</div>
+          </div>
+        </div>
       </div>
+      {/*    */}
+    </div>
+  );
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
